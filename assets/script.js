60bb93377d9cf1b69af1dbaa72b77f9247694324
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-10-15 17:31:03 -0500

    created js for application

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,35 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+  var currentHour = Number(dayjs().format('H'));
+
+  $('.time-block').each(function() {
+    var hourValue = Number($(this).attr("id"));
+
+    if (currentHour > hourValue) {
+      $(this).removeClass("present future").addClass("past");
+    } else if (currentHour < hourValue) {
+      $(this).removeClass("past present").addClass("future");
+    } else {
+      $(this).removeClass("past future").addClass("present");
+    }
+  });
+
+  var currentTime = dayjs().format('dddd, MMM D');
+  $('#display-4').text(currentTime);
+
+  $(".saveBtn").on("click", saveToLocalStorage);
+
+  function saveToLocalStorage() {
+    var blockId = $(this).parent().attr("id");
+    var blockDescription = $(this).siblings(".description").val();
+    localStorage.setItem(blockId, blockDescription);
+  }
+
+  for (var i = 9; i <= 16; i++) {
+    $(`#${i} .descritption`).var(localStorage.getItem(`${i}`))
+  }
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
